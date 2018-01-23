2b73ac25f0ce587160601cad64834d2b1a77e678
armfazh <armfazh@gmail.com>
2018-01-23 18:27:51 -0200

    removing array

--- code/c/aesni_optimized/haraka.h
+++ code/c/aesni_optimized/haraka.h
@@ -50,26 +50,26 @@ ALIGN u128 rc[40];
   s2 = _mm_aesenc_si128(s2, rc[rci + 6]); \
   s3 = _mm_aesenc_si128(s3, rc[rci + 7]); \
 
-//#define AES4_4x(s0, s1, s2, s3, rci) \
-  s0[0] = _mm_aesenc_si128(s0[0], rc[rci + 0]); s1[0] = _mm_aesenc_si128(s1[0], rc[rci + 0]);s2[0] = _mm_aesenc_si128(s2[0], rc[rci + 0]); s3[0] = _mm_aesenc_si128(s3[0], rc[rci + 0]);\
-  s0[1] = _mm_aesenc_si128(s0[1], rc[rci + 1]); s1[1] = _mm_aesenc_si128(s1[1], rc[rci + 1]);s2[1] = _mm_aesenc_si128(s2[1], rc[rci + 1]); s3[1] = _mm_aesenc_si128(s3[1], rc[rci + 1]);\
-  s0[2] = _mm_aesenc_si128(s0[2], rc[rci + 2]); s1[2] = _mm_aesenc_si128(s1[2], rc[rci + 2]);s2[2] = _mm_aesenc_si128(s2[2], rc[rci + 2]); s3[2] = _mm_aesenc_si128(s3[2], rc[rci + 2]);\
-  s0[3] = _mm_aesenc_si128(s0[3], rc[rci + 3]); s1[3] = _mm_aesenc_si128(s1[3], rc[rci + 3]);s2[3] = _mm_aesenc_si128(s2[3], rc[rci + 3]); s3[3] = _mm_aesenc_si128(s3[3], rc[rci + 3]);\
-  s0[0] = _mm_aesenc_si128(s0[0], rc[rci + 4]); s1[0] = _mm_aesenc_si128(s1[0], rc[rci + 4]);s2[0] = _mm_aesenc_si128(s2[0], rc[rci + 4]); s3[0] = _mm_aesenc_si128(s3[0], rc[rci + 4]);\
-  s0[1] = _mm_aesenc_si128(s0[1], rc[rci + 5]); s1[1] = _mm_aesenc_si128(s1[1], rc[rci + 5]);s2[1] = _mm_aesenc_si128(s2[1], rc[rci + 5]); s3[1] = _mm_aesenc_si128(s3[1], rc[rci + 5]);\
-  s0[2] = _mm_aesenc_si128(s0[2], rc[rci + 6]); s1[2] = _mm_aesenc_si128(s1[2], rc[rci + 6]);s2[2] = _mm_aesenc_si128(s2[2], rc[rci + 6]); s3[2] = _mm_aesenc_si128(s3[2], rc[rci + 6]);\
-  s0[3] = _mm_aesenc_si128(s0[3], rc[rci + 7]); s1[3] = _mm_aesenc_si128(s1[3], rc[rci + 7]);s2[3] = _mm_aesenc_si128(s2[3], rc[rci + 7]); s3[3] = _mm_aesenc_si128(s3[3], rc[rci + 7]);\
+#define AES4_4x_my(rci) \
+  s00 = _mm_aesenc_si128(s00, rc[rci + 0]); s10 = _mm_aesenc_si128(s10, rc[rci + 0]); s20 = _mm_aesenc_si128(s20, rc[rci + 0]); s30 = _mm_aesenc_si128(s30, rc[rci + 0]);\
+  s01 = _mm_aesenc_si128(s01, rc[rci + 1]); s11 = _mm_aesenc_si128(s11, rc[rci + 1]); s21 = _mm_aesenc_si128(s21, rc[rci + 1]); s31 = _mm_aesenc_si128(s31, rc[rci + 1]);\
+  s02 = _mm_aesenc_si128(s02, rc[rci + 2]); s12 = _mm_aesenc_si128(s12, rc[rci + 2]); s22 = _mm_aesenc_si128(s22, rc[rci + 2]); s32 = _mm_aesenc_si128(s32, rc[rci + 2]);\
+  s03 = _mm_aesenc_si128(s03, rc[rci + 3]); s13 = _mm_aesenc_si128(s13, rc[rci + 3]); s23 = _mm_aesenc_si128(s23, rc[rci + 3]); s33 = _mm_aesenc_si128(s33, rc[rci + 3]);\
+  s00 = _mm_aesenc_si128(s00, rc[rci + 4]); s10 = _mm_aesenc_si128(s10, rc[rci + 4]); s20 = _mm_aesenc_si128(s20, rc[rci + 4]); s30 = _mm_aesenc_si128(s30, rc[rci + 4]);\
+  s01 = _mm_aesenc_si128(s01, rc[rci + 5]); s11 = _mm_aesenc_si128(s11, rc[rci + 5]); s21 = _mm_aesenc_si128(s21, rc[rci + 5]); s31 = _mm_aesenc_si128(s31, rc[rci + 5]);\
+  s02 = _mm_aesenc_si128(s02, rc[rci + 6]); s12 = _mm_aesenc_si128(s12, rc[rci + 6]); s22 = _mm_aesenc_si128(s22, rc[rci + 6]); s32 = _mm_aesenc_si128(s32, rc[rci + 6]);\
+  s03 = _mm_aesenc_si128(s03, rc[rci + 7]); s13 = _mm_aesenc_si128(s13, rc[rci + 7]); s23 = _mm_aesenc_si128(s23, rc[rci + 7]); s33 = _mm_aesenc_si128(s33, rc[rci + 7]);\
 
   //AES4(s0[0], s0[1], s0[2], s0[3], rci); \
   //AES4(s1[0], s1[1], s1[2], s1[3], rci); \
   //AES4(s2[0], s2[1], s2[2], s2[3], rci); \
   //AES4(s3[0], s3[1], s3[2], s3[3], rci);
 
- #define AES4_4x_my(rci)\
-    AES4(s00,s01,s02,s03,rci);MIX4(s00, s01, s02, s03);\
-    AES4(s10,s11,s12,s13,rci);MIX4(s10, s11, s12, s13);\
-    AES4(s20,s21,s22,s23,rci);MIX4(s20, s21, s22, s23);\
-    AES4(s30,s31,s32,s33,rci);MIX4(s30, s31, s32, s33);
+ //#define AES4_4x_my(rci)\
+    AES4(s00,s01,s02,s03,rci);\
+    AES4(s10,s11,s12,s13,rci);\
+    AES4(s20,s21,s22,s23,rci);\
+    AES4(s30,s31,s32,s33,rci);
  
  
  #define AES4_4x(s0, s1, s2, s3, rci) \
